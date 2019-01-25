ba8f6c7d88bf7e5fe068bb1bca6838d6f460086f
Welling LaGrone <welling.lagrone@bhspecialty.com>
2019-01-25 11:43:55 -0500

    Update Extract to allow for environments

--- read/config.go
+++ read/config.go
@@ -3,6 +3,7 @@ package read
 type ReadConfig struct {
 	UsePreview  bool
 	SpaceID     string
+	Environment string
 	AccessToken string
 	Locale      string
 	// e.g. 200 items per page
--- read/contentful.go
+++ read/contentful.go
@@ -1,12 +1,14 @@
 package read
 
 import (
+	"fmt"
 	"io"
-	"strconv"
 )
 
 const previewURL string = "https://preview.contentful.com"
-const URL string = "https://cdn.contentful.com"
+const contentURL string = "https://cdn.contentful.com"
+const contentURLTemplate = "/spaces/%s/environments/%s/content_types?access_token=%s&limit=200&locale=%s"
+const itemsURLTemplate = "/spaces/%s/environments/%s/entries?access_token=%s&limit=200&locale=%s&skip=%d"
 
 type Contentful struct {
 	Getter     Getter
@@ -15,24 +17,18 @@ type Contentful struct {
 
 // Types will use Contentful's content_types endpoint to retrieve all content types from contentful
 func (c *Contentful) Types() (rc io.ReadCloser, err error) {
-
-	return c.get("/spaces/" +
-		c.ReadConfig.SpaceID + "/content_types?access_token=" +
-		c.ReadConfig.AccessToken + "&limit=200&locale=" +
-		c.ReadConfig.Locale)
+	url := fmt.Sprintf(contentURLTemplate, c.ReadConfig.SpaceID, c.ReadConfig.Environment, c.ReadConfig.AccessToken, c.ReadConfig.Locale)
+	return c.get(url)
 }
 
 // Items will use Contentful's entires endpoint to retrieve all 'items' from contetnful
 func (c *Contentful) Items(skip int) (rc io.ReadCloser, err error) {
-
-	return c.get("/spaces/" +
-		c.ReadConfig.SpaceID + "/entries?access_token=" +
-		c.ReadConfig.AccessToken + "&limit=200&locale=" +
-		c.ReadConfig.Locale + "&skip=" + strconv.Itoa(skip))
+	url := fmt.Sprintf(itemsURLTemplate, c.ReadConfig.SpaceID, c.ReadConfig.Environment, c.ReadConfig.AccessToken, c.ReadConfig.Locale, skip)
+	return c.get(url)
 }
 
 func (c *Contentful) get(endpoint string) (rc io.ReadCloser, err error) {
-	urlBase := URL
+	urlBase := contentURL
 	if c.ReadConfig.UsePreview {
 		urlBase = previewURL
 	}
